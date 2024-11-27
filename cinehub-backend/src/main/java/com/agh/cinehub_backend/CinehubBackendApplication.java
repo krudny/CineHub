e55d9c62c7dbf84f26884309ccb91fb6f00ea92d
Kamil Rudny <k.rudny1@gmail.com>
2024-11-27 17:02:51 +0100

    Starting backend files

---
+++ cinehub-backend/src/main/java/com/agh/cinehub_backend/CinehubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agh.cinehub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinehubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinehubBackendApplication.class, args);
+	}
+
+}
