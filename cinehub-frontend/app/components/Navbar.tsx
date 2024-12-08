1424190137c07710fd6c73c0d7481304fe50359c
Kamil Rudny <k.rudny1@gmail.com>
2024-12-08 22:06:15 +0100

    Responsive navbar

---
+++ cinehub-frontend/app/components/Navbar.tsx
@@ -0,0 +1,31 @@
+export default function Navbar() {
+  return (
+    <div className="w-full flex flex-col md:flex-row justify-between items-center text-neutral-100 px-16 ">
+      <div className="flex py-6 font-oswald text-2xl md:text-4xl">
+        <p className="p-1 md:py-1.5">Cine</p>
+        <p className="bg-orange-500 rounded-lg md:mx-1 p-1 md:p-1.5">Hub</p>
+      </div>
+      <div className="flex items-center font-oswald text-md md:text-2xl gap-x-10">
+        <p className="relative group">
+          Menu 1
+          <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-neutral-100 transition-all duration-300 group-hover:w-full"></span>
+        </p>
+        <p className="relative group">
+          Menu 2
+          <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-neutral-100 transition-all duration-300 group-hover:w-full"></span>
+        </p>
+        <p className="relative group">
+          Menu 3
+          <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-neutral-100 transition-all duration-300 group-hover:w-full"></span>
+        </p>
+        <p className="relative group">
+          Menu 4
+          <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-neutral-100 transition-all duration-300 group-hover:w-full"></span>
+        </p>
+        {/*<span className="material-icons mr-8 cursor-pointer">*/}
+        {/*  <p className="text-4xl">search</p>*/}
+        {/*</span>*/}
+      </div>
+    </div>
+  );
+}
