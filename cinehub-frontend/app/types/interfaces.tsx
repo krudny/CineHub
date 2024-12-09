4b1753aa20a5e58322b492df90e568c8836528d5
Kamil Rudny <k.rudny1@gmail.com>
2024-12-09 01:29:48 +0100

    Code refactor

---
+++ cinehub-frontend/app/types/interfaces.tsx
@@ -0,0 +1,8 @@
+export default interface HeroProps {
+    url: string;
+    title: string;
+    rating: number;
+    duration: string;
+    year: number;
+    description: string;
+}
---
+++ cinehub-frontend/app/utils/data.tsx
@@ -0,0 +1,10 @@
+import HeroProps from "@/app/types/interfaces";
+
+export const heroMovie:HeroProps = {
+    url: "/the_witcher.jpg",
+    year: 2020,
+    rating: 7.9,
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    duration: "2:15",
+    title: "The Witcher"
+}
