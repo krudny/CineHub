9fef190d49b8a0e3efbf260f034b81422bd6dafc
Wojciech Orłowski <vemtorpl@gmail.com>
2025-01-18 23:30:18 +0100

    adding movie rating statistics with fast backend access

--- cinehub-backend/src/main/java/com/agh/cinehub_backend/controller/StatisticsController.java
+++ cinehub-backend/src/main/java/com/agh/cinehub_backend/controller/StatisticsController.java
@@ -1,8 +1,14 @@
 package com.agh.cinehub_backend.controller;
 
 import com.agh.cinehub_backend.model.Movie;
+import com.agh.cinehub_backend.model.MovieRating;
+import com.agh.cinehub_backend.repository.MovieRatingsRepository;
+import com.agh.cinehub_backend.service.MovieRatingsService;
 import com.agh.cinehub_backend.service.StatisticsService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -15,6 +21,7 @@ import java.util.Map;
 public class StatisticsController {
 
     private final StatisticsService statisticsService;
+    private final MovieRatingsService movieRatingsService;
 
     @GetMapping("/getMostPopularMoviesEver/{quantity}")
     public List<Movie> getMostPopularMoviesEver(@PathVariable Integer quantity){
@@ -27,6 +34,19 @@ public class StatisticsController {
     @GetMapping("/soldTickets/{movieId}")
     public Map<LocalDate, Integer> getSoldTicketsStatistics(@PathVariable Integer movieId) {
         return statisticsService.getSoldTicketsStatistics(movieId);
+    }
+
+    @GetMapping("/getBestRatingMovies")
+    public Page<MovieRating> getBestRatingMovies(@RequestParam(value="page") int page,
+                                                 @RequestParam(value="size", defaultValue = "10") int size){
+        Pageable pageable = PageRequest.of(page, size);
+        return movieRatingsService.getMovieRatings(pageable);
+    }
+
+    @GetMapping("/getBestRatingMoviesWithLimit/{limit}")
+    public List<MovieRating> getBestRatingMovies(@PathVariable Integer limit){
+
+        return movieRatingsService.getMovieRatingsWithLimit(limit);
     }
 
 }
--- cinehub-backend/src/main/java/com/agh/cinehub_backend/service/StatisticsQuartz/QuartzConfig.java
+++ cinehub-backend/src/main/java/com/agh/cinehub_backend/service/StatisticsQuartz/QuartzConfig.java
@@ -8,6 +8,28 @@ import org.springframework.context.annotation.Configuration;
 public class QuartzConfig {
 
     @Bean
+    public JobDetail ratingsCalculatorJobDetail() {
+        return JobBuilder.newJob(MovieRatingsJob.class)
+                .withIdentity("MovieRatingsJob")
+                .storeDurably()
+                .build();
+    }
+    @Bean
+    public Trigger ratingsCalculatorJobTrigger() {
+        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
+//                .withIntervalInHours(24)
+                .withIntervalInMinutes(1)
+                .repeatForever();
+
+        return TriggerBuilder.newTrigger()
+                .forJob(ratingsCalculatorJobDetail())
+                .withIdentity("ratingsCalculatorTrigger")
+                .withSchedule(scheduleBuilder)
+                .startNow()
+                .build();
+    }
+
+    @Bean
     public JobDetail statisticsJobDetail() {
         return JobBuilder.newJob(StatisticsJob.class)
                 .withIdentity("statisticsJob")
